4dfacedd634fb5501d1a605b3001b807378042d8
BrandonSGC <brangmz20@gmail.com>
2024-07-22 18:15:27 -0600

    Project Initialization

---
+++ src/router.tsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter } from "react-router-dom"
+import { Layout } from "./layouts";
+import { NewProduct, Products } from "./views";
+
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout/>,
+    // Elements inside of children property are children 
+    // from the element. In this case <Layout />
+    children: [
+      {
+        element: <Products />,
+        index: true, //this is for let react know that we want 
+      },             // to render this element in the index ('/')
+      {
+        path: '/products/new',
+        element: <NewProduct/>
+      }
+    ]
+  },
+
+]);
